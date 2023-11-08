2d441a460ee3da4231df13ab347c5946d74f8d68
Bozhi Liu <bozhi.liu@hotmail.com>
2023-11-08 14:36:28 +0100

    Three different solutions

--- Leetcode 169.py
+++ Leetcode 169.py
@@ -8,3 +8,38 @@ class Solution:
                 ans = num
             count += (1 if num == ans else -1)         
         return ans
+    
+
+    
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+}
+
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap();
+        for(int num : nums){
+            if(!map.containsKey(num))
+                map.put(num, 1);
+            else{
+                map.put(num, map.getOrDefault(num, 1) + 1);
+            }  
+        }
+
+        int max = 0;
+        int result = 0;
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            int key = e.getKey();
+            int value = e.getValue();
+            if(max < value){
+                max = value;
+                result = key;
+            }
+        }        
+        return result;
+    }
+}
